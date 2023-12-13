de9a48d6130c0209f25d2f696468cf0ff06925a7
Kendall Weihe <kendallw@squareup.com>
2023-12-13 09:40:57 -0500

    Query postgres for offerings and return result

--- ftl/ftl-module-offerings/src/main/kotlin/ftl/offerings/Offerings.kt
+++ ftl/ftl-module-offerings/src/main/kotlin/ftl/offerings/Offerings.kt
@@ -1,19 +1,34 @@
 package ftl.offerings
 
+import ftl.lib.Json
 import ftl.lib.PostgresClient
+import tbdex.sdk.protocol.models.Offering
 import xyz.block.ftl.Context
 import xyz.block.ftl.Verb
 
 data class OfferingsRequest(val name: String)
 data class OfferingsResponse(val message: String)
 
+typealias GetOfferingsRequest = Unit
+data class GetOfferingsResponse(val offerings: String)
+
 class Offerings {
   private var postgresClient = PostgresClient()
 
   @Verb
-  fun echo(context: Context, req: OfferingsRequest): OfferingsResponse {
-    postgresClient.ping()
+  fun getOfferings(context: Context, req: GetOfferingsRequest): GetOfferingsResponse {
+    val offerings: MutableList<Offering> = mutableListOf()
+
+    val stmt = postgresClient.connection.createStatement()
+    val rs = stmt.executeQuery("SELECT * FROM offering")
+
+    while (rs.next()) {
+      val offering = Offering.parse(rs.getString("offering"))
+      offerings.add(offering)
+    }
+
     postgresClient.close()
-    return OfferingsResponse(message = "Hello, ${req.name}!")
+
+    return GetOfferingsResponse(Json.stringify(offerings))
   }
 }
