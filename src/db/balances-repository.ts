4424068348375b14e8b3799d6ece5b75bf48652c
Mic Neale <michael.neale@gmail.com>
2024-06-07 18:10:56 +1000

    adding in stored balances

--- src/db/balances-repository.ts
+++ src/db/balances-repository.ts
@@ -1,6 +1,8 @@
-import type { BalancesApi } from '@tbdex/http-server'
+import type { BalancesApi, Quote } from '@tbdex/http-server'
 import { Balance } from '@tbdex/http-server'
 import { config } from '../config.js'
+
+let available = '1000'
 
 export class _BalancesRepository implements BalancesApi {
   async getBalances({ requesterDid }): Promise<Balance[]> {
@@ -8,13 +10,23 @@ export class _BalancesRepository implements BalancesApi {
     const bal = Balance.create({
       data: {
         currencyCode: 'USDC',
-        available: '1000',
+        available: available,
       },
       metadata: {
         from: config.pfiDid.uri,
       }
     })
     return [bal]
+  }
+
+  withdraw(quote: Quote) {
+    // substract this from available
+    available = (parseFloat(available) - parseFloat(quote.data.payout.amount)).toString()
+  }
+
+  deposit(quote: Quote) {
+    // add this to available
+    available = (parseFloat(available) + parseFloat(quote.data.payin.amount)).toString()
   }
 }
 
